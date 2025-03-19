68d84e5591de69ba1e06744700555a8c82122437
Waiyanheinoo <waiyanheinoo99@gmail.com>
2025-03-20 03:35:13 +0700

    feat: add scrolling feature

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,12 +1,23 @@
+import React from "react";
 import Button from "./Button";
-import NavList from "./NavList";
 import { Link } from "react-router-dom";
 
-const Navbar = () => {
+const Navbar = ({ sectionRefs }) => {
+  // Function to handle smooth scrolling
+  const scrollToSection = (ref) => {
+    ref.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
   return (
-    <header className="flex justify-between items-center bg-[#E8F5E9] h-24 px-12 w-full shadow-md z-50">
+    <header className="flex justify-between items-center bg-[#E8F5E9] h-24 px-12 w-full shadow-md z-50 sticky top-0">
       {/* Logo */}
-      <a href="#">
+      <a
+        href="#"
+        onClick={(e) => {
+          e.preventDefault();
+          scrollToSection(sectionRefs.hero);
+        }}
+      >
         <img
           className="rounded-full w-20 h-20"
           alt="NutriTrack logo"
@@ -17,23 +28,44 @@ const Navbar = () => {
       {/* Navigation */}
       <nav>
         <ul className="flex items-center gap-8 text-lg font-medium">
-          <NavList text={"Home"} />
-          <NavList text={"Features"} />
-          <NavList text={"Testimonials"} />
+          <li>
+            <a
+              href="#hero"
+              onClick={(e) => {
+                e.preventDefault();
+                scrollToSection(sectionRefs.hero);
+              }}
+            >
+              Home
+            </a>
+          </li>
+          <li>
+            <a
+              href="#features"
+              onClick={(e) => {
+                e.preventDefault();
+                scrollToSection(sectionRefs.features);
+              }}
+            >
+              Features
+            </a>
+          </li>
+          <li>
+            <a
+              href="#testimonials"
+              onClick={(e) => {
+                e.preventDefault();
+                scrollToSection(sectionRefs.testimonials);
+              }}
+            >
+              Testimonials
+            </a>
+          </li>
           <Link to="/signup">
-            <Button
-              className="w-full"
-              text="Sign Up"
-              onClick={() => console.log("Sign Up Clicked")}
-            />
+            <Button className="w-full" text="Sign Up" />
           </Link>
           <Link to="/login">
-            <Button
-              className="w-full"
-              variant="secondary"
-              text="Log In"
-              onClick={() => console.log("Log In Clicked")}
-            />
+            <Button className="w-full" variant="secondary" text="Log In" />
           </Link>
         </ul>
       </nav>

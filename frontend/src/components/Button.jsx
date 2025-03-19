8bda640c86f8135013bf8959233b3389d5b6b749
Pyae Sone Shin Thant <pssthant@gmail.com>
2025-03-19 22:39:24 +0700

    fix: fix button color var

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -6,11 +6,11 @@ const Button = ({
   className = "",
 }) => {
   const baseStyles =
-    "px-6 py-2 font-semibold rounded-lg transition-all duration-200 cursor-pointer";
+    "px-6 py-2 font-semibold rounded-lg transition-all duration-200 shadow-md cursor-pointer";
 
   const variants = {
     primary: "bg-teal-500 text-white hover:bg-teal-600",
-    secondary: "bg-gray-300 text-black hover:bg-gray-400",
+    secondary: "bg-[#F6FBE9] text-black hover:bg-[#E1F3C0]",
     outline: "border-1 border-black text-green-500",
   };
 
